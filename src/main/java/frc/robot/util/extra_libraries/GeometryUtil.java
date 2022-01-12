5814a391bf333974dc430fa3c1fa5a7bb71c42a9
Pineapple42 <kleymanyy@gmail.com>
2022-01-12 14:33:25 -0600

    Changed package

--- src/main/java/frc/robot/util/extra_libraries/GeometryUtil.java
+++ src/main/java/frc/robot/util/extra_libraries/GeometryUtil.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.extra_libraries;
+package frc.robot.util.extra_libraries;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
