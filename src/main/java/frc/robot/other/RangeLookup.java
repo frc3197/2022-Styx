fc4ed4fcac0eb2eb5987c25a6fa773be5e553969
YuriKleyman <40747797+YuriKleyman@users.noreply.github.com>
2022-02-28 07:10:09 -0600

    Added RangePairs in increments of 5

--- src/main/java/frc/robot/other/RangeLookup.java
+++ src/main/java/frc/robot/other/RangeLookup.java
@@ -16,71 +16,105 @@ public class RangeLookup {
             case 70:
                 rangePair = new RangePair(-390, 3000);
                 break;
+            case 75:
+                rangePair = new RangePair(-410, 3200);
+                break;
             case 80:
                 rangePair = new RangePair(-430, 3500);
                 break;
+            case 85:
+            rangePair = new RangePair(-450, 3500);
+            break;
             case 90:
                 rangePair = new RangePair(-470, 3500);
                 break;
+            case 95:
+            rangePair = new RangePair(-465, 3500);
+            break;
             case 100:
                 rangePair = new RangePair(-500, 3600);
                 break;
+            case 105:
+            rangePair = new RangePair(-520, 3750);
+            break;
             case 110:
                 rangePair = new RangePair(-540, 3900);
                 break;
+                
+            case 115:
+            rangePair = new RangePair(-600, 3900);
+            break;
             case 120:
-                rangePair = new RangePair(-650, 4200);
+                rangePair = new RangePair(-670, 4300);
+                break;
+                case 125:
+                rangePair = new RangePair(-675, 4300);
                 break;
             case 130:
                 rangePair = new RangePair(-680, 4300);
                 break;
+                
+            case 135:
+            rangePair = new RangePair(-700, 4350);
+            break;
             case 140:
                 rangePair = new RangePair(-720, 4400);
                 break;
+                
+            case 145:
+            rangePair = new RangePair(-790, 4800);
+            break;
             case 150:
                 rangePair = new RangePair(-850, 5100);
+                break;
+                case 155:
+                rangePair = new RangePair(-820, 5200);
                 break;
             case 160:
                 rangePair = new RangePair(-780, 5300);
                 break;
-                case 170:
-                rangePair = new RangePair(-850, 6100);
+                case 165:
+                rangePair = new RangePair(-820, 5700);
                 break;
         
-                case 180:
+            case 170:
+                rangePair = new RangePair(-850, 6200);
+                break;
+
+            case 180:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 190:
+            case 190:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 200:
+            case 200:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 210:
+            case 210:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 220:
+            case 220:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 230:
+            case 230:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 240:
+            case 240:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 250:
+            case 250:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 260:
+            case 260:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 270:
+            case 270:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 280:
+            case 280:
                 rangePair = new RangePair(0, 5800);
                 break;
-                case 290:
+            case 290:
                 rangePair = new RangePair(0, 5800);
                 break;
             default:
@@ -91,15 +125,15 @@ public class RangeLookup {
     }
 
     public static int normalizeRange(double range) {
-        return (int) (10 * (Math.round(range / 10)));
+        return (int) (5 * (Math.round(range / 5)));
     }
 
     public static double convertLLYtoRange(double ty) {
-        if(ty != 0){
-        return (Constants.subsystems.hood.HubHeight - Constants.subsystems.hood.LLHeight)
-                / Math.tan(Units.degreesToRadians(Constants.subsystems.hood.LLAng + ty));
+        if (ty != 0) {
+            return (Constants.subsystems.hood.HubHeight - Constants.subsystems.hood.LLHeight)
+                    / Math.tan(Units.degreesToRadians(Constants.subsystems.hood.LLAng + ty));
+        } else {
+            return 0;
+        }
     }
-    else{
-        return 0;
-    }
-}}
+}
